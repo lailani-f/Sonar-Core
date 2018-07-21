d983cc9234c5f4f8281cac79262c94658db832ab
lailani-f <lailani.faylinn@gmail.com>
2018-07-21 14:10:57 +0200

    Fixed SyncedValueList elements being appended to the existing list instead of being replaced on sync. Hack to truncate synced connection list.

--- src/main/java/sonar/core/sync/SyncedValueList.java
+++ src/main/java/sonar/core/sync/SyncedValueList.java
@@ -1,5 +1,7 @@
 package sonar.core.sync;
 
+import sonar.core.SonarCore;
+
 import java.util.List;
 
 public class SyncedValueList<V> extends SonarControlledList<V> implements ISyncValue<List<V>> {
@@ -11,6 +13,18 @@ public class SyncedValueList<V> extends SonarControlledList<V> implements ISyncV
         super(type, watcher, value);
         this.handler = new SyncHandlerList<>(SyncRegistry.getHandler(type));
         this.key = key;
+    }
+
+
+    @Override
+    public boolean setValueInternal(List<V> set) {
+        int size = set.size();
+        if ("8".equals(getTagName()) && size > 6) {
+            set.subList( 6, size - 1 ).clear();
+            SonarCore.logger.info("Truncated abnormally large connection list 'Size: " + size + "'");
+        }
+        value.clear();
+        return super.setValueInternal(set);
     }
 
     @Override
